13ff088171596e7475db40da1d7b19c7f91a8762
L3stevam <00001115533319sp@al.educacao.sp.gov.br>
2024-12-11 20:14:33 -0300

    primeira versão do meu projeto

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao (
+   'Programação',
+   'O que é JavaScript?',
+   'O JavaScript é uma línguagem de programação',
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O Pacífico',
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+
+criaCartao (
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma línguagem de estilização',
+)
